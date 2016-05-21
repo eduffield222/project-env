7363a657c215845e9c285e250e604b734dc62fbb
Evan Duffield <evan@dashpay.io>
2016-05-21 10:32:12 -0700

    ubuntu dir

--- evan-ubuntu-develop.bash
+++ evan-ubuntu-develop.bash
@@ -9,8 +9,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/misc.bash
 
+HOMEDIR="/home/ubuntu"
 DASHLOG="mnbudget,mnpayments"
-DASHDEBUG=""
+DASHDEBUG=" --"
 DASHBINARY="./dashd"
 DASHDIR="dash/testnet"
 DASHBINARY="./dashd"
@@ -49,8 +50,8 @@ cmd_dash()
   if [ "$1 $2" = "set mode" ]; then return; fi;
 
   #---- generic commands
-  if [ "$1" = "start" ]; then $DASHDEBUG ~/$DASHDIR/bin/$DASHBINARY --datadir=~/$DASHDIR/data --debug=$DASHLOG -logthreadnames; return; fi;
-  if [ "$1 $2" = "hard start" ]; then $DASHDEBUG ~/$DASHDIR/bin/$DASHBINARY --datadir=~/$DASHDIR/data --debug=$DASHLOG -reindex -logthreadnames; return; fi;
+  if [ "$1" = "start" ]; then $DASHDEBUG ~/$DASHDIR/bin/$DASHBINARY --datadir=$HOMEDIR/$DASHDIR/data --debug=$DASHLOG -logthreadnames; return; fi;
+  if [ "$1 $2" = "hard start" ]; then $DASHDEBUG ~/$DASHDIR/bin/$DASHBINARY --datadir=$HOMEDIR/$DASHDIR/data --debug=$DASHLOG -reindex -logthreadnames; return; fi;
   if [ "$1" = "cd" ]; then cd ~/$DASHDIR/src; return; fi;
 
   #---- tail $DASHDIR/network/debug.log
@@ -61,8 +62,10 @@ cmd_dash()
   array=$@;
   array="${array[@]:3}";
 
+  echo $array;
+  echo "$HOMEDIR/$DASHDIR/data"
   #----- cli commands
-  if [ "$1" = "cli" ]; then cd ~/$DASHDIR/bin && ./dash-cli --datadir=~/$DASHDIR/data $array; return; fi;
+  if [ "$1" = "cli" ]; then cd ~/$DASHDIR/bin && ./dash-cli --datadir=$HOMEDIR/$DASHDIR/data $array; return; fi;
 
   echo "unknown dash command. see 'dash help'"
 }
