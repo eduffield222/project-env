d1305af51574ed9c54001eacf35eb0843c292922
Evan Duffield <evan@dashpay.io>
2016-05-21 10:03:06 -0700

    update ubuntu bash

--- evan-ubuntu-develop.bash
+++ evan-ubuntu-develop.bash
@@ -9,13 +9,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/misc.bash
 
-HOMEUSER="evan"
 DASHLOG="mnbudget,mnpayments"
-#DASHDEBUG="lldb --"
-DASHDEBUG=""
-DASHNETWORK="testnet" #mainnet, testnet, regtest
-DASHDIR="./dash/develop"
-DASHBINARY="./dash/bin/develop/dashd"
+DASHDEBUG="lldb --"
+DASHBINARY="./dashd"
+DASHDIR="dash/testnet"
+DASHBINARY="./dashd"
+DASHNETWORK="testnet"
 
 cmd_dash()
 {
@@ -47,26 +46,25 @@ cmd_dash()
   #---- setup binaries
   if [ "$1 $2 $3" = "set mode qt" ]; then DASHBINARY="./dash-qt";echo "qt mode on"; return; fi;
   if [ "$1 $2 $3" = "set mode daemon" ]; then DASHBINARY="./dashd";echo "daemon mode on"; return; fi;
-  if [ "$1 $2 $3" = "set mode testnet" ]; then DASHBINARY="./dash-qt";echo "qt mode on"; return; fi;
-  if [ "$1 $2 $3" = "set mode mainnet" ]; then DASHBINARY="./dashd";echo "daemon mode on"; return; fi;
   if [ "$1 $2" = "set mode" ]; then return; fi;
 
   #---- generic commands
-  if [ "$1" = "start" ]; then $DASHDEBUG $DASHBINARY --datadir=/Users/$HOMEUSER/$DASHDIR --debug=$DASHLOG -logthreadnames; return; fi;
-  if [ "$1 $2" = "hard start" ]; then $DASHDEBUG $DASHBINARY --datadir=/Users/$HOMEUSER/$DASHDIR --debug=$DASHLOG -reindex -logthreadnames; return; fi;
-  if [ "$1" = "cd" ]; then cd ~/Desktop/dash-develop; return; fi;
+  if [ "$1" = "start" ]; then $DASHDEBUG ~/$DASHDIR/bin/$DASHBINARY --datadir=$DIR/../../data --debug=$DASHLOG -logthreadnames; return; fi;
+  if [ "$1 $2" = "hard start" ]; then $DASHDEBUG ~/$DASHDIR/bin/$DASHBINARY --datadir=$DIR/../../data --debug=$DASHLOG -reindex -logthreadnames; return; fi;
+  if [ "$1" = "cd" ]; then cd ~/$DASHDIR/src; return; fi;
 
   #---- tail $DASHDIR/network/debug.log
-  if [ "$1 $DASHNETWORK" = "tail testnet" ]; then cd ~/$DASHDIR/testnet3 && tail -f debug.log ; return; fi;
-  if [ "$1 $DASHNETWORK" = "tail mainnet" ]; then cd ~/$DASHDIR/ && tail -f debug.log ; return; fi;
-  if [ "$1 $DASHNETWORK" = "tail regtest" ]; then cd ~/$DASHDIR/regtest && tail -f debug.log ; return; fi;
+  if [ "$1 $DASHNETWORK" = "tail testnet" ]; then cd ~/$DASHDIR/data/testnet3 && tail -f debug.log ; return; fi;
+  if [ "$1 $DASHNETWORK" = "tail mainnet" ]; then cd ~/$DASHDIR/data && tail -f debug.log ; return; fi;
+  if [ "$1 $DASHNETWORK" = "tail regtest" ]; then cd ~/$DASHDIR/data/regtest && tail -f debug.log ; return; fi;
 
   array=$@;
   array="${array[@]:3}";
 
   echo $array;
+  echo "$DIR/../../data"
   #----- cli commands
-  if [ "$1" = "cli" ]; then cd ~/Desktop/dash-develop && ./dash-cli --datadir=/Users/$HOMEUSER/$DASHDIR $array; return; fi;
+  if [ "$1" = "cli" ]; then cd ~/$DASHDIR/bin && ./dash-cli --datadir=$DIR/../../data $array; return; fi;
 
   echo "unknown dash command. see 'dash help'"
 }
