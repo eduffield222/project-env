f78967a1135227985246a0819f00704bb8e0d48d
Evan Duffield <evan@dashpay.io>
2016-05-31 15:47:48 -0700

    change user to ubuntu

--- evan-ubuntu-develop.bash
+++ evan-ubuntu-develop.bash
@@ -9,7 +9,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/misc.bash
 
-HOMEUSER="evan"
+HOMEUSER="ubuntu"
 DASHLOG="mnbudget,mnpayments"
 DASHDEBUG=""
 DASHBINARY="./dashd"
